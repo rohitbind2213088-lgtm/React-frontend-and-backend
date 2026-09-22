cee6f9580f24d6c74c6f292466616c3408bf95dd
rohitbind2213088-lgtm <rohitbind2213088@gmail.com>
2026-09-22 16:47:57 +0530

    hello world

--- backend/class.js
+++ backend/class.js
@@ -1,12 +1,27 @@
-const express=require("express");
-const app=express();
-const PORT=3000;
+const express = require("express");
+const mysql = require("mysql2");
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
+const app = express();
+const PORT = 3000;
 
+const server = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "student",
+    password: ""
+});
+server.connect((err) => {
+    if (err) {
+        console.log("MySQL connection failed:", err.message);
+    } else {
+        console.log("MySQL connected successfully!");
+    }
 });
 
-app.listen(PORT,()=>{
-    console.log(`server running at http://localhost:${PORT}`);
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
